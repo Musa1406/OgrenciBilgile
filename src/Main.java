2a0bec35ac6352b826fb9758eaaa181d5ef7f9b5
Musa1406 <91537282+Musa1406@users.noreply.github.com>
2021-09-28 14:45:07 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        //sysout ctrl space
+		System.out.println("Merhaba Musa");
+		System.out.println(" ");
+		System.out.println("Sevdigin Spor Dallarý");
+		System.out.println("Futbol,");
+		System.out.print("Bisiklet");
+		System.out.print(" ");
+		System.out.println("Sürmek");
+		System.out.println("Ve Basketbol Oynamak");
+		System.out.println(" ");
+		System.out.println("Bilgilerin");
+		System.out.println("sýnýfým 10-A");
+		System.out.println("Yas :"+15);
+		System.out.println("Boy :"+178);
+		System.out.println(" ");
+		System.out.print("Okudugun");
+		System.out.print(" ");
+		System.out.println("Okul");
+		System.out.println("Izzet Baysal Tekniki ve Mesleki Anadolu Lisesi");
+	}
+
+}
